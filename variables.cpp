1ac4dfc1b161bf6fe068f74e0d261caf92a5c87e
Jesus <orbau07@gmail.coom>
2021-09-06 15:55:00 -0500

    Constantes y variables
    Constantes y variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> // Standar Input Ouput 
+
+/* 
+Descripcion: Uso de variables
+Autor: Jesus Rivera
+Date: 6-9-2021
+*/
+
+void main (void){
+	// tipodato Identificador=ValorInicial;
+	
+	int piolin=0; // Declarando Entero e inicializando Variable
+	const int silvestre=0; // Declarando constante entera e inicializando en 0
+	
+	// printf("Etiqueta formatos", variable);
+	
+	printf("Mi variable piolin tiene el valor de %i",piolin);
+	printf("Mi variable silvestre tiene el valor de %i",silvestre);
+}
